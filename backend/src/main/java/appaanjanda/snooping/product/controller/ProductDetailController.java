a244112a82fb41f9812d3546d01e6ebb7a82a8c4
최영태 <dudxo7721@naver.com>
2023-09-15 02:29:41 +0900

    ✨ feat : [상품 상세 조회 초기 구현] | [최영태] | [ProductController, ProductService, ProductDetailDto, ElasticsearchConfig]

---
+++ backend/src/main/java/appaanjanda/snooping/product/controller/ProductDetailController.java
@@ -0,0 +1,28 @@
+package appaanjanda.snooping.product.controller;
+
+
+import appaanjanda.snooping.jwt.MemberInfo;
+import appaanjanda.snooping.jwt.MembersInfo;
+import appaanjanda.snooping.member.entity.Member;
+import appaanjanda.snooping.product.dto.ProductDetailDto;
+import appaanjanda.snooping.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductDetailController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{majorCategory}/{productId}")
+    public ProductDetailDto getProductDetail(@PathVariable String majorCategory, @PathVariable String productId, @MemberInfo MembersInfo membersInfo){
+
+        return productService.getProductById(membersInfo.getId(), majorCategory, productId);
+
+    }
+}
