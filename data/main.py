e908f06b5f14b5047f5ea6f6d27eb297acca6ed1
Giggle1998 <rlvy98@naver.com>
2023-09-18 17:17:29 +0900

    ✨ feat : [쿠팡 가구 크롤러 개발] | [이기표] | [coupang.py]

--- data/main.py
+++ data/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from apscheduler.schedulers.background import BackgroundScheduler
-from coupang import coupang_products, coupang_products_all, coupang_products_digital
+from coupang import coupang_products, coupang_products_all, coupang_products_digital, coupang_products_furniture
 from naver import naver_products, naver_products_all, naver_products_digital, naver_products_furniture, naver_products_necessaries, naver_products_food
 import pandas as pd
 import requests
@@ -28,8 +28,12 @@ def coupang_all():
     return {"message": "Crawling completed!"}
 
 @app.get("/coupang/digital")
-def naver_digital():
+def coupang_digital():
     return coupang_products_digital()
+
+@app.get("/coupang/furniture")
+def coupang_furniture():
+    return coupang_products_furniture()
 ########################################## 네이버 #####################################################
 
 # 네이버 쇼핑 API 함수
