eaf6f64bb969f1021d7df3adafb49f2a9453fade
최영태 <dudxo7721@naver.com>
2023-09-19 02:18:21 +0900

    ✨ feat : [최근 본 상품 구현] | [최영태] | [RecentProduct, RecentProductRepository, ProductController]

---
+++ backend/src/main/java/appaanjanda/snooping/domain/product/entity/RecentProduct.java
@@ -0,0 +1,28 @@
+package appaanjanda.snooping.domain.product.entity;
+
+import appaanjanda.snooping.domain.member.entity.Member;
+import appaanjanda.snooping.global.common.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+// 최근 본 상품
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecentProduct extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String productId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+}
