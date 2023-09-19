71b1116d238cd0e920042112683f8c3602055937
강민승 <k4minseung@gmail.com>
2023-09-19 17:33:45 +0900

    ✏ modify : [time zone 서울로 설정] | [강민승] | [SnoopingApplication]

--- backend/src/main/java/appaanjanda/snooping/SnoopingApplication.java
+++ backend/src/main/java/appaanjanda/snooping/SnoopingApplication.java
@@ -1,14 +1,21 @@
 package appaanjanda.snooping;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class SnoopingApplication {
 
 	public static void main(String[] args) {
+		// timezone 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SnoopingApplication.class, args);
+
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println("현재시간 " + now);
 	}
 
 }
