d4ffac826d1345cec92ad2246de906e661eda52a
강민승 <k4minseung@gmail.com>
2023-09-25 10:44:38 +0900

    ✨ feat : [카드 조회] | [강민승] | [CardController, CardRepository, CardService, CardResponse]

--- backend/src/main/java/appaanjanda/snooping/domain/card/repository/CardRepository.java
+++ backend/src/main/java/appaanjanda/snooping/domain/card/repository/CardRepository.java
@@ -2,6 +2,7 @@ package appaanjanda.snooping.domain.card.repository;
 
 import java.util.List;
 
+import appaanjanda.snooping.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import appaanjanda.snooping.domain.card.entity.MyCard;
@@ -16,6 +17,7 @@ public interface CardRepository extends JpaRepository<MyCard, Long> {
 	@Query("delete from MyCard m where m.id = :id")
 	void delete(@Param("id") Long id);
 
+	void deleteAllByMember(Member member);
 
 	List<MyCard> findMyCardByMemberId(Long id);
 	void deleteMyCardByCardType(String cardType);
