defd46b53f669d8321c786c59ccf539d5f0d03ac
강민승 <k4minseung@gmail.com>
2023-09-25 12:58:08 +0900

    🛠 fix : [채팅 stomp 잠시 해제] | [강민승] | [chat]

---
+++ backend/src/main/java/appaanjanda/snooping/domain/firebase/FCMNotificationService.java
@@ -0,0 +1,48 @@
+package appaanjanda.snooping.domain.firebase;
+
+import appaanjanda.snooping.domain.member.entity.Member;
+import appaanjanda.snooping.domain.member.repository.MemberRepository;
+import appaanjanda.snooping.global.error.code.ErrorCode;
+import appaanjanda.snooping.global.error.exception.BadRequestException;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class FCMNotificationService {
+
+    private final FirebaseMessaging firebaseMessaging;
+    private final MemberRepository memberRepository;
+
+    public String sendNotificationByToken(FCMNotificationRequestDto requestDto) {
+
+        Member member = memberRepository.findById(requestDto.getTargetUserId()).orElseThrow(()
+                -> new BadRequestException(ErrorCode.NOT_EXISTS_USER_ID));
+
+        if (member.getFirebaseToken() != null) {
+            Notification notification = Notification.builder()
+                    .setTitle(requestDto.getTitle())
+                    .setBody(requestDto.getBody())
+                    .build();
+
+            Message message = Message.builder()
+                    .setToken(member.getFirebaseToken())
+                    .setNotification(notification)
+                    .build();
+
+            try {
+                firebaseMessaging.send(message);
+            } catch (FirebaseMessagingException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return "Token 없머";
+        }
+        return "뭐";
+    }
+}
