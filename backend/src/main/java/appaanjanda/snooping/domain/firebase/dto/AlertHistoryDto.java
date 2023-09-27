923ddcf6c33a1a612b0471c4c67da4bde63230d3
Giggle1998 <rlvy98@naver.com>
2023-09-27 20:14:02 +0900

    ✨ feat : [알림 목록 조회 Dto 생성] | [이기표] | [AlertHistoryDto]

---
+++ backend/src/main/java/appaanjanda/snooping/domain/firebase/dto/AlertHistoryDto.java
@@ -0,0 +1,11 @@
+package appaanjanda.snooping.domain.firebase.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AlertHistoryDto {
+    private String title;
+    private String body;
+}
